5a5c2b9bf2a433dc18b92b6cf4f03fcce294956b
Maximilian <maximilian.achleitner@htl-wels.at>
2020-10-08 13:16:49 +0200

    added loop

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,23 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int i = 0;
 
-        try {
-            System.out.println("Zahl 1:");
-            int i1 = sc.nextInt();
+        while(i==0) {
+            try {
+                System.out.println("Zahl 1:");
+                int i1 = Integer.parseInt(sc.next());
 
-            System.out.println("Zahl 2:");
-            int i2 = sc.nextInt();
+                System.out.println("Zahl 2:");
+                int i2 = Integer.parseInt(sc.next());
 
-            i = i1 + i2;
+                i = i1 + i2;
 
-            System.out.println("Ergebnis: " + i);
-        }catch(Exception e){
-            System.out.println("Es wurde ein nicht kompatibler Wert angegeben!");
+            } catch (Exception e) {
+                System.out.println("Es wurde ein nicht kompatibler Wert angegeben!");
+            }
         }
+
+
+        System.out.println("Ergebnis: " + i);
 
 
     }
